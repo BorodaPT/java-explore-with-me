0e1b1b20b46a6719e2b3cbac990a346914260cbd
Boroda <via4eslavboroda@yandex.ru>
2023-09-15 15:44:27 +0300

    check-style

--- main_ewm/main_ewm_service/src/main/java/ru/practicum/services/events/RepositoryEvent.java
+++ main_ewm/main_ewm_service/src/main/java/ru/practicum/services/events/RepositoryEvent.java
@@ -4,10 +4,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.services.events.model.Event;
-import ru.practicum.services.events.model.StatusEvent;
 
 import java.time.LocalDateTime;
 import java.util.List;
